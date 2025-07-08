a78d8b3870a624fbdf4e29475e894b3edfa1ac91
zeke <ezekiel.ravidat@gmail.com>
2025-07-08 16:55:45 +0200

    fix background layer

--- components/BackgroundLayer.tsx
+++ components/BackgroundLayer.tsx
@@ -1,6 +1,6 @@
 export default function BackgroundLayer() {
   return (
-    <div className="isolate overflow-hidden min-h-[700px] absolute inset-0 -z-20 bg-[#f7f7f9]">
+    <div className="isolate overflow-hidden max-h-[700px] absolute inset-0 -z-20 bg-[#f7f7f9]">
       {/* Top-right blob */}
       <div className="absolute inset-0 bg-[radial-gradient(50%_140%_at_100%_0%,#f6e4bc_0%,#f6e4bcaa_40%,transparent_90%)] bg-no-repeat" />
 
